50a18607a8e914c010d43d3577ebf74d33203775
Ashish Mistry <ashishmistry19@yahoo.com>
2019-07-11 12:51:39 -0400

    Updated with Class and Object Functions

--- ObjectOrientedProg/RunFile.py
+++ ObjectOrientedProg/RunFile.py
@@ -2,7 +2,7 @@ import CannyEdgeDetector_OOP as CED
 import cv2
 from scipy import signal
 from matplotlib import pyplot as plt
-img = cv2.imread(r'C:\Users\Ashish\Desktop\Graduate School\Study\images\lena.png')
+img = cv2.imread(r'C\PATH\')
 RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 grayImage=cv2.cvtColor(RGB_img, cv2.COLOR_RGB2GRAY)
 
