982ecac1a11c40f8922426c93acf9ddd3f4f14df
Ashish Mistry <ashishmistry19@yahoo.com>
2019-07-11 19:39:59 -0400

    update

--- ObjectOrientedProg/RunFile.py
+++ ObjectOrientedProg/RunFile.py
@@ -1,9 +1,6 @@
 import CannyEdgeDetector_OOP as CED
-import cv2
-from scipy import signal
 from matplotlib import pyplot as plt
 from PIL import Image
-import PIL
 import os, os.path
 images=[]
 path= r'C:PATH_HERE'
@@ -14,13 +11,6 @@ for f in os.listdir(path):
     if extension not in imagetypes:
         continue
     images.append(Image.open(os.path.join(path,f)))
-
-
-
-# np.asarray(images[0])
-    
-#RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#grayImage=cv2.cvtColor(RGB_img, cv2.COLOR_RGB2GRAY)
 
 # image,size,sigma,weakpix,strongpix,lowthresh,highthresh)
 detector=CED.cannyedge(images,5,2,100,255,.05,.17)
